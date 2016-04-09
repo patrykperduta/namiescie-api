d7714ff80d4fe80a006231ad34bd26fcb08413a9
Michal Zalecki <michal@michalzalecki.com>
2016-04-09 17:41:11 +0200

    Almost full rewrite

--- index.js
+++ index.js
@@ -1,111 +1,113 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var uuid = require("node-uuid");
 
-
-var people = {
-    1: {
-        sex: "male",
-        topic: "beer",
-        position: {
-            lat: 51.084812,
-            lng: 17.013667
-        }
-    },
-    2: {
-        sex: "female",
-        topic: "beer",
-        position: {
-            lat: 51.087775,
-            lng: 17.013924
-        }
-    },
-    3: {
-        sex: "female",
-        topic: "beer",
-        position: {
-            lat: 51.087406,
-            lng: 17.007773
-        }
+var people = [
+  {
+    id: uuid.v4(),
+    sex: "male",
+    topic: "piwo",
+    position: {
+      lat: 51.084812,
+      lng: 17.013667
     }
-};
-
-var meetpoints = {
-    1: {
-        name: "Szynkarnia",
-        position: {
-            lat: 51.109328,
-            lng: 17.025007
-        }
+  },
+  {
+    id: uuid.v4(),
+    sex: "female",
+    topic: "piwo",
+    position: {
+      lat: 51.087775,
+      lng: 17.013924
     }
-}
-
-var sockets = {};
-
-
-function get_people_list(people){
-    return Object.keys(people)
-      .map(id => Object.assign({}, people[id], { id: id }))
-}
-
-function get_meetpoint(meetpoints) {
-    return meetpoints['1'];
-}
-
-function handle_intro(socket, payload) {
-    var person = {
-      sex: payload.sex,
-      position: payload.position,
-    };
-    var topic = payload.topic;
-    var id = payload.id;
-    person.socket = socket;
-    people[id] = person;
-
-    console.log('on_intro received:', JSON.stringify(payload));
-    io.emit('people', get_people_list());
-}
-
-
-function handle_ping(socket, payload) {
-    console.log('on_ping received:', JSON.stringify(payload));
-    var invited_id = payload.id;
-    var invited_socket = sockets[invited_id];
-    var topic = socket.topic;
-    var person = people[topic][socket.id];
-    invited_socket.emit('ping', person);
-}
-
-
-function handle_pong(socket, payload) {
-    console.log('on_pong received:', JSON.stringify(payload));
-    if (payload.accpeted == true) {
-      var accepted_person_id = payload.id;
-      var accepted_person_socket = sockets[accepted_person_id];
-      var topic = socket.topic;
-      var person = people[topic][socket.id];
-      var meetpoint = get_meetpoint();
-
-      accepted_person_socket.emit('meet', meetpoint);
-      socket.emit('meet', meetpoint);
+  },
+  {
+    id: uuid.v4(),
+    sex: "female",
+    topic: "piwo",
+    position: {
+      lat: 51.087406,
+      lng: 17.007773
     }
-    else {
-      // TODO: not acceted
+  }
+];
+
+var places = [
+  {
+    id: uuid.v4(),
+    name: "Szynkarnia",
+    position: {
+      lat: 51.109328,
+      lng: 17.025007
     }
+  }
+];
+
+function filterPeopleByTopic(people, topic) {
+  return people.filter(person => person.topic === topic);
 }
 
+function findById(collection, id) {
+  return collection.find(item => item.id === id);
+}
+
+function getNearestPlace(places, sourcePos, destinationPos) {
+  return places[0];
+}
+
+function serializePerson(person) {
+  var newPerson = Object.assign({}, person);
+  delete newPerson.socket;
+  return newPerson;
+}
+
+function handleDisconnect(socket) {
+  console.log("on disconnect");
+
+  people = people.filter(person => person.socket !== socket);
+  io.emit('people', people.map(serializePerson));
+}
+
+function handleIntro(socket, payload) {
+  console.log("on intro");
+
+  people.push(Object.assign({}, payload, { socket: socket }));
+  io.emit('people', filterPeopleByTopic(people, payload.topic).map(serializePerson));
+}
+
+function handleNotify(socket, payload) {
+    console.log("on notify");
+
+    const source = findById(people, payload.sourceId);
+    const destination = findById(people, payload.destinationId);
+    console.log(source.id, destination.id);
+    destination.socket.emit("getNotified", serializePerson(source));
+}
+
+function handleAcceptNotification(socket, payload) {
+    console.log("on acceptNotification");
+
+    if (!payload.accepted) return;
+
+    const source = findById(people, payload.sourceId);
+    const destination = findById(people, payload.destinationId);
+    const place = getNearestPlace(places, source.position, destination.position);
+
+    source.socket.emit("place", place);
+    destination.socket.emit("place", place);
+}
 
 io.on('connection', function(socket) {
-    socket.on('intro', payload => handle_intro(socket, payload));
-    socket.on('ping', payload => handle_ping(socket, payload));
-    socket.on('pong', payload => handle_pong(socket, payload));
+    socket.on('intro', payload => handleIntro(socket, payload));
+    socket.on('notify', payload => handleNotify(socket, payload));
+    socket.on('disconnect', payload => handleDisconnect(socket, payload));
+    socket.on('acceptNotification', payload => handleAcceptNotification(socket, payload));
 });
-
 
 http.listen(process.env.PORT || 3000, function() {
     console.log('listening on *:', process.env.PORT || 3000);
 });
-
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
